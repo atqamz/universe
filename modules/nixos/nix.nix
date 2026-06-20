9b9c9b1b076fa1caa2fe4231c08a93f0d999ac92
Atqa Munzir <atqamz@gmail.com>
2026-06-20 14:28:06 +0700

    add cachix substituter for host rebuilds

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,7 +1,13 @@
 _: {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    extra-substituters = [ "https://atqamz-universe.cachix.org" ];
+    extra-trusted-public-keys = [
+      "atqamz-universe.cachix.org-1:XTxFJDQxSXjQ+mu7oHYN8udmwDkCccjIbihvb8ZNJKU="
+    ];
+  };
   nixpkgs.config.allowUnfree = true;
 }
