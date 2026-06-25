db69f4d3dc475b18ea6ce2a0c73baab29a333960
Atqa Munzir <atqamz@gmail.com>
2026-06-25 10:52:24 +0700

    use thunar as default file manager

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }:
+let
+  archiveManager = pkgs.file-roller.overrideAttrs (previous: {
+    buildInputs = builtins.filter (package: package != pkgs.nautilus) previous.buildInputs;
+    mesonFlags = (previous.mesonFlags or [ ]) ++ [
+      "-Dnautilus-actions=disabled"
+      "-Dpackagekit=false"
+    ];
+  });
+in
 {
   programs = {
     hyprland = {
@@ -6,6 +15,13 @@
       withUWSM = true;
     };
     nix-ld.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs; [
+        thunar-archive-plugin
+        thunar-volman
+      ];
+    };
   };
 
   services = {
@@ -19,10 +35,13 @@
 
     printing.enable = true;
 
+    tumbler.enable = true;
     udisks2.enable = true;
     gvfs.enable = true;
   };
   security.polkit.enable = true;
+
+  environment.systemPackages = [ archiveManager ];
 
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
