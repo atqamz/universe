a85d2afecb3e29359f523564d0723070be13f093
Atqa Munzir <atqamz@gmail.com>
2026-07-06 02:01:03 +0700

    feat(boot): quiet boot console output

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -3,5 +3,15 @@ _: {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     resumeDevice = "/dev/disk/by-partlabel/disk-main-swap";
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "loglevel=3"
+      "systemd.show_status=auto"
+      "rd.systemd.show_status=auto"
+      "rd.udev.log_level=3"
+      "udev.log_level=3"
+    ];
   };
 }
