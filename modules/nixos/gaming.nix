e913b9169081f20a7f356c5cb0c5879ace637ec0
Atqa Munzir <atqamz@gmail.com>
2026-07-06 09:39:15 +0700

    add gaming power profile and default balance_power epp
    gamemode start raises cpu to 32/38w, sets performance epp, caps gpu clock 1300;
    gamemode end restores 25w undervolt, 1540 gpu lock, balance_power epp.
    cpu-epp service defaults idle to balance_power for cooler daily use.

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,4 +1,37 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  gamingPower = pkgs.writeShellApplication {
+    name = "gaming-power";
+    runtimeInputs = [
+      config.hardware.nvidia.package.bin
+      pkgs.systemd
+    ];
+    text = ''
+      rapl=/sys/class/powercap/intel-rapl:0
+      epp() { for f in /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference; do echo "$1" >"$f"; done; }
+      case "''${1:-}" in
+        on)
+          systemctl stop undervolt.timer undervolt.service
+          echo 32000000 >"$rapl/constraint_0_power_limit_uw"
+          echo 38000000 >"$rapl/constraint_1_power_limit_uw"
+          epp performance
+          nvidia-smi -lgc 210,1300
+          nvidia-smi -pl 35
+          ;;
+        off)
+          systemctl restart gpu-undervolt.service
+          systemctl restart undervolt.service
+          systemctl start undervolt.timer
+          epp balance_power
+          ;;
+        *)
+          echo "usage: gaming-power on|off" >&2
+          exit 1
+          ;;
+      esac
+    '';
+  };
+in
 {
   programs = {
     steam = {
@@ -18,11 +51,42 @@
 
     gamescope.enable = true;
 
-    gamemode.enable = true;
+    gamemode = {
+      enable = true;
+      settings.custom = {
+        start = "/run/wrappers/bin/sudo ${gamingPower}/bin/gaming-power on";
+        end = "/run/wrappers/bin/sudo ${gamingPower}/bin/gaming-power off";
+      };
+    };
+  };
+
+  security.sudo.extraRules = [
+    {
+      users = [ "atqa" ];
+      commands = [
+        {
+          command = "${gamingPower}/bin/gaming-power";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
+
+  systemd.services.cpu-epp = {
+    description = "bias CPU to balance_power EPP by default";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeShellScript "cpu-epp" ''
+        for f in /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference; do echo balance_power >"$f"; done
+      '';
+    };
   };
 
   environment.systemPackages = with pkgs; [
     mangohud
     protonup-qt
+    gamingPower
   ];
 }
