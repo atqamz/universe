7e35fbd042e92ac0e294a04dc93054baded0696c
Atqa Munzir <atqamz@gmail.com>
2026-07-12 20:45:10 +0700

    prevent mimeapps.list backup collision on home-manager switch

--- modules/home/file-management.nix
+++ modules/home/file-management.nix
@@ -41,6 +41,11 @@
 
   home.sessionVariables.GTK_USE_PORTAL = "1";
 
+  # writableMimeApps turns the managed symlink into a real file, so each switch HM
+  # would back it up to mimeapps.list.bak and collide with the previous backup;
+  # force overwrites in place instead of backing up.
+  xdg.configFile."mimeapps.list".force = true;
+
   home.activation.writableMimeApps = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     file="${config.home.homeDirectory}/.config/mimeapps.list"
     if [ -L "$file" ]; then
