d4705318bae074c0b492757a861a2c1b46acc859
Atqa Munzir <atqamz@gmail.com>
2026-08-03 06:17:46 +0700

    feat(home): use the work email inside ~/github/yes2games
    A gitdir include, so commits in org checkouts carry the right identity without
    a per-repo config.

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,4 +1,5 @@
-_: {
+{ config, ... }:
+{
   programs.git = {
     enable = true;
     settings.user = {
@@ -9,5 +10,11 @@ _: {
       key = "8AD7D4A302EE6771";
       signByDefault = true;
     };
+    includes = [
+      {
+        condition = "gitdir:${config.home.homeDirectory}/github/yes2games/";
+        contents.user.email = "atqa@yes2games.com";
+      }
+    ];
   };
 }
