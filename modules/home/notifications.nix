cbfc97831af18be22723d37d0bcac184d15611be
Atqa Munzir <atqamz@gmail.com>
2026-08-15 16:01:10 +0700

    no-mistakes(test): Add bounded declarative Mako user service

--- modules/home/notifications.nix
+++ modules/home/notifications.nix
@@ -1,5 +1,22 @@
-_: {
+{ config, ... }:
+{
   services.mako.enable = true;
+
+  systemd.user.services.mako = {
+    Unit = {
+      Description = "Mako notification daemon";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+      StartLimitBurst = 5;
+      StartLimitIntervalSec = "60s";
+    };
+    Service = {
+      ExecStart = "${config.services.mako.package}/bin/mako";
+      Restart = "on-failure";
+      RestartSec = "2s";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
 
   universe.doctor.activeUserServices = [ "mako" ];
 }
